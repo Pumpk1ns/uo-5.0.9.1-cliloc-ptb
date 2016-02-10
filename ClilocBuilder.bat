2afafe399e3c55741d8ed7b6f2b96dc83024042d
Victor Nogueira <felladrin@gmail.com>
2016-02-09 23:27:06 -0300

    Add ClilocBuilder - A simple program to build the file Cliloc.ptb. (Requires .NET Framework). To use it, just run ClilocBuilder.bat, which will compile, run and then delete the executable. After that you should see the Cliloc.ptb file generated on the folder.

---
+++ ClilocBuilder.bat
@@ -0,0 +1,3 @@
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc /optimize /unsafe /t:exe /out:ClilocBuilder.exe /recurse:ClilocBuilder.cs
+ClilocBuilder.exe
+del ClilocBuilder.exe
---
+++ ClilocBuilder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ClilocBuilder
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            var fi = new FileInfo(string.Concat("Cliloc.ptb"));
+            FileStream fs;
+
+            try
+            {
+                fs = new FileStream(fi.FullName, FileMode.Create);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+
+            var bw = new BinaryWriter(fs);
+            bw.Write(new byte[] { 2, 0, 0, 0, 1, 0 });
+
+            using (var sr = new StreamReader("CliLoc.csv"))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if ((line = line.Trim()).Length == 0 || line.StartsWith("#") || line.StartsWith("Number;"))
+                        continue;
+                    
+                    try
+                    {
+                        string[] split = line.Split(';');
+
+                        if (split.Length < 2)
+                            continue;
+
+                        int number = int.Parse(split[0].Trim());
+                        string text = split[1].Trim();
+                        byte[] textBytes = Encoding.UTF8.GetBytes(text);
+
+                        bw.Write(Convert.ToUInt32(number));
+                        bw.Write((byte)0);
+                        bw.Write(Convert.ToUInt16(textBytes.Length));
+                        bw.Write(textBytes);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+
+            fs.Close();
+            bw.Close();
+        }
+    }
+}
